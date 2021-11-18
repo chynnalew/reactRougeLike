a625c11c03d6018113f3d546cb903816cdcc2996
chynnalew <chynnalew@yahoo.com>
2021-11-18 14:57:37 -0800

    add useRef, useEffect hooks

--- src/components/reactRogue.js
+++ src/components/reactRogue.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const ReactRouge = ({ width, height, tilesize }) => {
+  const canvasReference = React.useRef();
+  React.useEffect(() => {
+  console.log('draw to canvas');
+  })
+  return (
     <canvas
-      width={width * tilesize}
-      height={height * tilesize}
-      style={{border: '1px solid black'}}
-    ></canvas>
+      ref={canvasReference}
+       width={width * tilesize}
+       height={height * tilesize}
+       style={{border: '1px solid black'}}
+     ></canvas>
+  )
 }
 
 export default ReactRouge;
