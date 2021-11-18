20b0a4aebff51625e4686a4fc16de5fd9bf29e02
chynnalew <chynnalew@yahoo.com>
2021-11-18 15:04:36 -0800

    draw square with canvasRenderingContext2D

--- src/components/reactRogue.js
+++ src/components/reactRogue.js
@@ -3,7 +3,11 @@ import React from 'react';
 const ReactRouge = ({ width, height, tilesize }) => {
   const canvasReference = React.useRef();
   React.useEffect(() => {
-  console.log('draw to canvas');
+    console.log('draw to canvas');
+    const context = canvasReference.current.getContext('2d');
+    context.clearRect(0, 0, width * tilesize, height * tilesize)
+    context.fillStyle = '#000';
+    context.fillRect(12, 22, 16, 16)
   })
   return (
     <canvas
